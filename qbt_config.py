00314fe8d029d2800608f2ce4ebe6cee5a451fa7
Joreis <joao.reis@protonmail.ch>
2020-09-03 11:07:33 +0200

    minor adjustments

--- qbt_config.py
+++ qbt_config.py
@@ -6,4 +6,4 @@ series_folder    = "/home/joreis/plex-media/Series"
 movies_folder    = "/home/joreis/plex-media/Movies"
 doc_folder       = "/home/joreis/plex-media/Documentaries"
 
-qbt_client = qbittorrentapi.Client(host='192.168.0.164', port=8080, username='admin', password='1ns2deout')
+qbt_client = qbittorrentapi.Client(host='192.168.0.164', port=8080, username='admin', password='adminadmin')
