eb2693753e9a3e33c63a45168677bca52ea5f985
Joreis <joao.reis@protonmail.ch>
2020-09-03 10:58:50 +0200

    completed Movies move

--- move_torrents.py
+++ move_torrents.py
@@ -31,8 +31,7 @@ def move_torrents():
     serie_file_name = []
     serie_season    = []
     serie_episode   = []
-    serie_hash      = []
-    
+    serie_hash      = []    
 
     movie_title     = []
     movie_file_name = []
@@ -44,12 +43,12 @@ def move_torrents():
 
 
     for torrents in t_list:
+
         if torrents[1] == "Anime":
             parsed_torrents = PTN.parse(torrents[0])
             anime_title.append(parsed_torrents['title'])
             anime_file_name.append(torrents[0])
             anime_hash.append(torrents[3])
-
     
         if torrents[1] == "Movies":
             parsed_torrents = PTN.parse(torrents[0])
@@ -64,7 +63,6 @@ def move_torrents():
             serie_episode.append(parsed_torrents['episode'])
             serie_file_name.append(torrents[0])
             serie_hash.append(torrents[3])
-
         
         if torrents[1] == "Documentary":
             parsed_torrents = PTN.parse(torrents[0])
@@ -72,31 +70,63 @@ def move_torrents():
             doc_file_name.append(torrents[0])
             doc_hash.append(torrents[3])
 
-
+    
 
     anime_list  = list(zip(anime_title, anime_file_name,anime_hash))
     series_list = list(zip(serie_title,serie_season, serie_episode, serie_file_name,serie_hash))
     docs_list   = list(zip(doc_title, doc_file_name,doc_hash))
     movies_list = list(zip(movie_title, movie_file_name,movie_hash))
 
-    # print(anime_list)
-    # print("............")
-    # print(series_list)
+    print(anime_list)
+    print("............")
+    print(series_list)
     # print("............")
     # print(docs_list)
-    # print("............")
-    # print(movies_list)
+    print("............")
+    print(movies_list)
 
-    
+ 
+
     for files in movies_list:
-      os.rename(downloads_folder + "/" + files[1], movies_folder + "/" + files[0] )
+
+        try:
+            os.rename(downloads_folder + "/" + files[1], movies_folder + "/" + files[0] )
+            print("Movie" + files[0] + " was moved to " + movies_folder)
+
+            qbt_client.torrents_delete(torrent_hashes=str(files[2]), delete_files= False)
+            print("torrent" + files[0] + "was deleted")
+        
+        except OSError as err:
+            print(err)
+      
       
 
 
-    # for files in series_list:
-    #     os.mkdir(series_folder + "/" + files[0])
-    #     os.mkdir(series_folder + "/" + files[0] + "/" "season " + str(files[1]))
-    #     os.rename(downloads_folder + "/" + files[3], series_folder + "/" + files[0] + "/" "season " + str(files[1]) + "/" + files[3])
+    for files in series_list:
+
+         try:
+            os.mkdir(series_folder + "/" + files[0])
+            os.mkdir(series_folder + "/" + files[0] + "/" "season " + str(files[1]))
+            os.rename(downloads_folder + "/" + files[3], series_folder + "/" + files[0] + "/" "season " + str(files[1]) + "/" + files[3])
+            print("Serie" + files[0] + " was moved to " + movies_folder)
+
+            qbt_client.torrents_delete(torrent_hashes=str(files[4]), delete_files= False)
+            print("torrent" + files[0] + "was deleted")
+            
+         except OSError as err:
+            print(err)
+
     
+    #for files in anime_list:
+
+    #   try:
+    #        os.rename(downloads_folder + "/" + files[1], movies_folder + "/" + files[0] )
+    #        print("Movie" + files[0] + " was moved to " + movies_folder)
+
+    #        qbt_client.torrents_delete(torrent_hashes=str(files[2]), delete_files= False)
+    #        print("torrent" + files[0] + "was deleted")
+        
+    #    except OSError as err:
+    #        print(err)
 
 move_torrents()
--- qbt_config.py
+++ qbt_config.py
@@ -1,9 +1,9 @@
 import qbittorrentapi
 
 downloads_folder = "/home/joreis/Downloads"
-anime_folder     = "/home/joreis/Media/Anime"
-series_folder    = "/home/joreis/Media/Series"
-movies_folder    = "/home/joreis/Media/Movies"
-doc_folder       = "/home/joreis/Media/Documentaries"
+anime_folder     = "/home/joreis/plex-media/Anime"
+series_folder    = "/home/joreis/plex-media/Series"
+movies_folder    = "/home/joreis/plex-media/Movies"
+doc_folder       = "/home/joreis/plex-media/Documentaries"
 
-qbt_client = qbittorrentapi.Client(host='localhost', port=8080)
+qbt_client = qbittorrentapi.Client(host='192.168.0.164', port=8080, username='admin', password='1ns2deout')
